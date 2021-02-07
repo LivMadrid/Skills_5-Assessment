c242e3c9731d1e24f9a14a871d0859735cfc80c8
Olivia <liv.madrid14@gmail.com>
2021-02-07 21:51:33 +0000

    Complete Part 1 and Complete 2/3 Part 2

--- model.py
+++ model.py
@@ -10,10 +10,42 @@ db = SQLAlchemy()
 
 class Human(db.Model):
     """Data model for a human."""
+    #define column attributes for table Humans 
+    __tablename__ = 'humans'
 
+    human_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    fname = db.Column(db.String, nullable=False)
+    lname = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+
+    #unique so user can't enter same email twice and have two human_id numbers
+    #decided to put human_id and email for repr because both are unique and easily identifiable 
+    def __repr__(self):
+        """Show Human Info"""
+        return f'<Human human_id={self.human_id} email={self.email}>'
 
 class Animal(db.Model):
     """Data model for an animal."""
+#define column attributes for table Animals 
+    __tablename__ = 'animals'
+
+    animal_id = db.Column(db.Integer,
+                         autoincrement=True,
+                         primary_key=True)
+    human_id = db.Column(db.Integer, db.ForeignKey('humans.human_id'))
+    name = db.Column(db.String, nullable=False)
+    animal_species = db.Column(db.String, nullable=False)
+    birth_year = db.Column(db.Integer)
+    #birth_year is nullable 
+    #Because animals could have same name I decided to include species as well to differentiate 
+
+    human = db.relationship('Human', backref='animals')
+
+    def __repr__(self):
+        """Show Animal Info"""
+        return f'<Animal animal_id={self.animal_id} name={self.name} animal_species={self.animal_species}>'
 
 
 def connect_to_db(app):
--- query.py
+++ query.py
@@ -10,29 +10,36 @@ from model import db, Human, Animal
 def get_human_2():
     """Return the human with the id 2."""
 
-
+    # human_2 = (db.session.query(Human).filter(Human.human_id == 2).one())
+    # return human_2
+    return print(db.session.query(Human).filter(Human.human_id == 2).one())
+    
 def get_first_fish():
     """Return the FIRST animal with the species 'fish'."""
 
+    return print(db.session.query(Animal).filter(Animal.animal_species == 'fish').first())
 
 def get_young_animals():
     """Return all animals that were born after 2015.
 
     Do NOT include animals without birth years.
     """
-
+    return db.session.query(Animal).filter(Animal.birth_year > '2015', Animal.birth_year != None).all()
 
 def get_j_names():
     """Return the humans with first names that start with 'J'."""
 
+    return print(db.session.query(Human).filter(Human.fname.like('J%')).all())
 
 def get_null_bdays():
     """Return all animals whose birth years are NULL."""
 
+    return db.session.query(Animal).filter(Animal.birth_year == None).all()
 
 def get_fish_or_rabbits():
     """Return all animals whose species is 'fish' OR 'rabbit'."""
 
+    return print(db.session.query(Animal).filter(db.or_(Animal.animal_species.like('%fish%'), Animal.animal_species.like('%rabbit%'))).all())
 
 def print_directory():
     """Output a list of humans and their animals.
@@ -49,7 +56,18 @@ def print_directory():
     You may only use ONE query to retrieve initial data. (Hint: leverage a
     SQLAlchemy relationship to retrieve additional information)
     """
+    humans_and_animals = db.session.query(Human.fname,
+                    Human.lname,
+                    Human.human_id,
+                    Animal.name,
+                    Animal.animal_species).join(Animal)
+    
+    # all_humans_all_animals =  humans_and_animals.group_by(Human.fname, Human.lname, Human.human_id, Animal.name, Animal.animal_species)
 
+   
+
+    # for fname, lname, name in humans_and_animals :
+    #     return fname, lname, name
 
 def find_humans_by_animal_species(species):
     """Return a list of all humans who have animals of the given species.
@@ -62,7 +80,13 @@ def find_humans_by_animal_species(species):
     relationship! Also, you can pursue uniqueness in a Pythonic way --- you
     don't have to do it with pure SQLAlchemy)
     """
-
+    find_humans = db.session.query(Human.human_id).join(Animal).group_by(fname, lname).having(Animal.animal_species.like('%%'))
+    
+    # filter(Animal.animal_species.like('%%'))
+    find_humans.all()
+    for fname, lname in find_humans: 
+        return fname, lname
+    
 
 if __name__ == '__main__':
     from server import app
